2007ac89e43e4278e7a6d44ef78e2bcbbdd765af
Yasiru Lokesha <yasirulokesha01@gmail.com>
2022-04-21 11:03:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
   reactStrictMode: true,
 }
 
-// eslint: {
-//   // Warning: This allows production builds to successfully complete even if
-//   // your project has ESLint errors.
-//   ignoreDuringBuilds: true
-// }
+eslint = {
+  // Warning: This allows production builds to successfully complete even if
+  // your project has ESLint errors.
+  ignoreDuringBuilds: true
+}
